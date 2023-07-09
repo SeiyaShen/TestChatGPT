37d4a684aae4d0ecbd4b80151c90869c7e5b6c3d
SeiyaShen <64568858+SeiyaShen@users.noreply.github.com>
2023-07-09 18:16:23 +0800

    Update chatgpt_utils.py

--- chatgpt_utils.py
+++ chatgpt_utils.py
@@ -1,12 +1,10 @@
 # embedding_utils.py
 
 import openai
-import os
 import re
 import requests
 import sys
 from num2words import num2words
-import os
 import pandas as pd
 import numpy as np
 from openai.embeddings_utils import get_embedding, cosine_similarity
@@ -27,51 +25,7 @@ openai.api_base = RESOURCE_ENDPOINT
 #openai.api_version = "2022-12-01"
 openai.api_version = "2023-03-15-preview" 
 
-# search through the reviews for a specific product
-def search_docs( user_query, top_n = 3, to_print = False):
-    url = openai.api_base + "/openai/deployments?api-version=2022-12-01" 
 
-    r = requests.get(url, headers={"api-key": API_KEY})
-
-    # This assumes that you have placed the bill_sum_data.csv in the same directory you are running Jupyter Notebooks
-    df=pd.read_csv(os.path.join(os.getcwd(),r'C:\Users\seiya.z.shen\OneDrive - Avanade\Data_AI\Open_AI\bill_sum_data.csv')) 
-
-    # DataFrame called df_bills which will contain only the columns for text, summary, and title.
-    df_bills = df[['text', 'summary', 'title']]
-
-    #https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#evaluation-order-matters
-    pd.options.mode.chained_assignment = None 
-
-    df_bills['text']= df_bills["text"].apply(lambda x : normalize_text(x))
-
-    # need to remove any bills that are too long for the token limit (8192 tokens).
-    tokenizer = tiktoken.get_encoding("cl100k_base")
-    df_bills['n_tokens'] = df_bills["text"].apply(lambda x: len(tokenizer.encode(x)))
-    df_bills = df_bills[df_bills.n_tokens < 8192]
-
-    # To understand the n_tokens column a little more as well how text ultimately is tokenized, 
-    sample_encode = tokenizer.encode(df_bills.text[0]) 
-    decode = tokenizer.decode_tokens_bytes(sample_encode)
-
-    # engine should be set to the deployment name you chose when you deployed the text-embedding-ada-002 (Version 2) model
-    df_bills['ada_v2'] = df_bills["text"].apply(lambda x : get_embedding(x, engine = 'Seiya-embedding-ada-002' ))
-
-    embedding = get_embedding(
-        user_query,
-        engine = 'Seiya-embedding-ada-002' # engine should be set to the deployment name you chose when you deployed the text-embedding-ada-002 (Version 2) model
-    )
-    df_bills["similarities"] = df_bills.ada_v2.apply(lambda x: cosine_similarity(x, embedding))
-
-    res = (
-        df_bills.sort_values("similarities", ascending=False)
-        .head(top_n)
-    )
-    if res.empty:
-        return "Sorry, I cannot find the answer"
-    
-    if to_print:
-        print(res) # original is display(res)
-    return res["summary"].iloc[0]
 
 def get_answer( question ):
     model_engine = "gpt-35-turbo"
